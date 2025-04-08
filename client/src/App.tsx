aff08cb9c7b193fa7b2a5b23233dcbf69fbb106f
Erik Niilivirta <erik.niilivirta@gmail.com>
2025-04-09 02:11:04 +0300

    Add React Query for data fetching and implement API functions
    - Integrated `@tanstack/react-query` for improved data management.
    - Created `api.ts` with functions to fetch data for people and planets from the SWAPI.
    - Updated `App.tsx` to include `QueryClientProvider` for React Query context.
    - Modified `package.json` and `package-lock.json` to include `@tanstack/react-query` as a dependency.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { Navbar } from './components/Navbar'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 
 function App() {
+
+  const queryClient = new QueryClient()
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
