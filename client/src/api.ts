bcbe1763fa102fb166770333f08200ff329bc904
Erik Niilivirta <erik.niilivirta@gmail.com>
2025-04-22 13:31:26 +0300

    Add mock data for people and implement conditional fetching in API functions

--- client/src/api.ts
+++ client/src/api.ts
@@ -36,15 +36,104 @@ export interface Planet {
   url: string
 }
 
+const mockPeople: Person[] = [
+  {
+    name: 'Luke Skywalker',
+    height: '172',
+    mass: '77',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    birth_year: '19BBY',
+    gender: 'male',
+    homeworld: 'https://swapi.dev/api/planets/1/',
+    films: [
+      'https://swapi.dev/api/films/1/',
+      'https://swapi.dev/api/films/2/',
+      'https://swapi.dev/api/films/3/',
+      'https://swapi.dev/api/films/6/',
+    ],
+    species: [],
+    vehicles: ['https://swapi.dev/api/vehicles/14/', 'https://swapi.dev/api/vehicles/30/'],
+    starships: ['https://swapi.dev/api/starships/12/', 'https://swapi.dev/api/starships/22/'],
+    created: '2014-12-09T13:50:51.644000Z',
+    edited: '2014-12-20T21:17:56.891000Z',
+    url: 'https://swapi.dev/api/people/1/',
+  },
+  {
+    name: 'Leia Organa',
+    height: '150',
+    mass: '49',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'brown',
+    birth_year: '19BBY',
+    gender: 'female',
+    homeworld: 'https://swapi.dev/api/planets/2/',
+    films: [
+      'https://swapi.dev/api/films/1/',
+      'https://swapi.dev/api/films/2/',
+      'https://swapi.dev/api/films/3/',
+      'https://swapi.dev/api/films/6/',
+    ],
+    species: [],
+    vehicles: ['https://swapi.dev/api/vehicles/30/'],
+    starships: [],
+    created: '2014-12-10T15:20:09.791000Z',
+    edited: '2014-12-20T21:17:50.315000Z',
+    url: 'https://swapi.dev/api/people/5/',
+  },
+  {
+    name: 'Obi-Wan Kenobi',
+    height: '182',
+    mass: '77',
+    hair_color: 'auburn, white',
+    skin_color: 'fair',
+    eye_color: 'blue-gray',
+    birth_year: '57BBY',
+    gender: 'male',
+    homeworld: 'https://swapi.dev/api/planets/20/',
+    films: [
+      'https://swapi.dev/api/films/1/',
+      'https://swapi.dev/api/films/2/',
+      'https://swapi.dev/api/films/3/',
+      'https://swapi.dev/api/films/6/',
+    ],
+    species: [],
+    vehicles: [],
+    starships: ['https://swapi.dev/api/starships/48/', 'https://swapi.dev/api/starships/59/'],
+    created: '2014-12-10T16:16:29.192000Z',
+    edited: '2014-12-20T21:17:50.325000Z',
+    url: 'https://swapi.dev/api/people/10/',
+  },
+]
+
+// Environment variable or configuration to control using mocks
+// SWAPI isnt't working correctly, so we are using mocks
+const useMocks = true
+
 const getPeople = async (id: number): Promise<Person> => {
+  if (useMocks) {
+    const person = mockPeople.find((p) => p.url === `${apiUrl}/people/${id}/`)
+    if (!person) {
+      throw new Error('Person not found')
+    }
+
+    return person
+  }
   const response = await fetch(`${apiUrl}/people/${id}`)
 
   return response.json()
 }
 
 const getAllPeople = async (): Promise<Person[]> => {
+  if (useMocks) {
+    return mockPeople
+  }
+
   const response = await fetch(`${apiUrl}/people`)
   const data = await response.json()
+
   return data.results
 }
 
