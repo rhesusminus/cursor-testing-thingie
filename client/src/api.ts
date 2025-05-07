caac14a3d3c4c1894fe59153af68d29dc366c671
Erik Niilivirta <erik.niilivirta@gmail.com>
2025-05-08 00:36:59 +0300

    Update API endpoints to use local JSON files for fetching planets and people data

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,23 +1,23 @@
 import type { Planet, Person } from './types'
 
 export async function getPlanets(): Promise<Planet[]> {
-  const response = await fetch('https://swapi.dev/api/planets/')
+  const response = await fetch('/api/api/planets/all.json')
   const data = await response.json()
-  return data.results
+  return data
 }
 
 export async function getPlanetById(id: number): Promise<Planet> {
-  const response = await fetch(`https://swapi.dev/api/planets/${id}/`)
+  const response = await fetch(`/api/api/planets/${id}.json`)
   return response.json()
 }
 
 export async function getPersonById(id: number): Promise<Person> {
-  const response = await fetch(`https://swapi.dev/api/people/${id}/`)
+  const response = await fetch(`/api/api/people/${id}.json`)
   return response.json()
 }
 
 export async function getPeople(): Promise<Person[]> {
-  const response = await fetch('https://swapi.dev/api/people/')
+  const response = await fetch('/api/api/people/all.json')
   const data = await response.json()
-  return data.results
+  return data
 }
